6375dcc94a117ce71f4e87e094c86482ce165a6e
Georgi Iliev <georgi.iliev533@outlook.com>
2023-03-10 13:32:39 +0200

    Task 03 Completed

--- Practicum-SI/week03/task03.cpp
+++ Practicum-SI/week03/task03.cpp
@@ -1,63 +1,161 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+
+const char FILE_PATH[] = "file.dat";
 
 struct JobOffer {
     char company[26];
     int employees;
     short yearlyPaidLeave;
-    double projectCost;
+    double salary;
 };
 
-void readJobOffer(JobOffer &offer) {
+void readJobOffer(JobOffer& offer) {
     offer = JobOffer{};
     std::cin.getline(offer.company, 26);
-    std::cin >> offer.employees >> offer.yearlyPaidLeave >> offer.projectCost;
+    std::cin >> offer.employees >> offer.yearlyPaidLeave >> offer.salary;
+    std::cin.ignore(1);
+}
+
+void printJobOffer(const JobOffer& offer) {
+    std::cout << "Job Offer from: " << offer.company << std::endl;
+    std::cout << "Employees: " << offer.employees << std::endl;
+    std::cout << "Paid leave: " << offer.yearlyPaidLeave << "  Salary: " << offer.salary << std::endl;
+    std::cout << std::endl;
 }
 
 bool addJobOffers(const char* filePath) {
     size_t n;
+    std::cout << "Enter count: ";
     std::cin >> n;
     std::cin.ignore(1);
+
     JobOffer* offers = new JobOffer[n];
-    std::cout << "Reading elements:" << std::endl;
-    for(size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++) {
+        std::cout << "Enter data for item #" << (i+1) << std::endl;
         readJobOffer(offers[i]);
     }
 
-    size_t count = 0;
+    size_t count = 0, eof = 0;
 
     std::ifstream input(filePath, std::ios::in | std::ios::binary);
-    if(input.is_open()) {
-        input >> count;
+    if (input.is_open()) {
+        input.read((char*)&count, sizeof(count));
+        input.seekg(0, std::ios::end);
+        eof = input.tellg();
         input.close();
     }
 
-    std::ofstream output(filePath, std::ios::out | std::ios::binary);
-    if(!input.is_open()) {
-        return false;
+    std::ofstream output;
+    output.open(filePath, std::ios::in | std::ios::out | std::ios::ate | std::ios::binary);
+    if (!output.is_open()) {
+        output.open(filePath, std::ios::out | std::ios::binary);
+        if (!output.is_open()) {
+            return false;
+        }
     }
+
     count += n;
+    output.seekp(0, std::ios::beg);
     output.write((const char*)&(count), sizeof((count)));
-    output.seekp(0, std::ios::end);
-    for(int i = 0; i < n; i++) {
-        output.write((const char *)&offers[i], sizeof(offers[i]));
+    if(eof != 0) {
+        output.seekp(eof, std::ios::beg);
+    }
+    std::cout << "End is at: " << output.tellp() << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        output.write((const char*)&offers[i], sizeof(offers[i]));
     }
 
     output.close();
-    return false;
+    return true;
 }
 
-//- извежда на стандартния изход всички обяви със заплата по - висока от minSalary]
-void filterOffer(const char* filePath, size_t minSalary) {
-    
+bool filterOffer(const char* filePath, size_t minSalary) {
+    std::ifstream input(filePath, std::ios::in | std::ios::binary);
+    if(!input.is_open()) {
+        return false;
+    }
+
+    size_t count = 0;
+    JobOffer buffer{};
+    input.read((char *)&count, sizeof(count));
+    while(count-- ) {
+        input.read((char *)&buffer, sizeof(buffer));
+        if(buffer.salary > (double)minSalary) {
+            printJobOffer(buffer);
+        }
+    }
+
+    input.close();
+    return true;
 }
 
-//  - проверява дали съществува оферта от фирма
-void findOffer(const char* filePath, const char* name) {
+JobOffer* findOffer(const char* filePath, const char* name) {
+    std::ifstream input(filePath, std::ios::in | std::ios::binary);
+    if(!input.is_open()) {
+        return nullptr;
+    }
 
+    size_t count = 0;
+    JobOffer buffer{};
+    input.read((char *)&count, sizeof(count));
+    while(count-- ) {
+        input.read((char *)&buffer, sizeof(buffer));
+        if(std::strcmp(buffer.company, name) == 0) {
+            return new JobOffer{buffer};
+        }
+    }
+
+    input.close();
+    return nullptr;
 }
 
 int main() {
-    addJobOffers("file.dat");
-    return 0;
+    int n;
+    while(true) {
+        std::cout << "1. Save job offers to file\n" << "2. Filter offer by salary\n" << "3. Find offer by company\n";
+        std::cout << "Enter choice:";
+        std::cin >> n;
+        std::cin.ignore(1);
+        switch(n) {
+            case 1: {
+                if (addJobOffers(FILE_PATH)) {
+                    std::cout << "Saving was successful!" << std::endl;
+                }
+                else {
+                    std::cout << "Saving failed!" << std::endl;
+                }
+                break;
+            }
+            case 2: {
+                std::cout << "Enter salary:";
+                size_t salary;
+                std::cin >> salary;
+                std::cin.ignore(1);
+                filterOffer(FILE_PATH, salary);
+                break;
+            }
+            case 3: {
+                std::cout << "Enter company name:" << std::endl;
+                char name[1024];
+                std::cin.getline(name, 1024);
+                JobOffer* offer = findOffer(FILE_PATH, name);
+                if(offer != nullptr) {
+                    std::cout << "Found the job offer!:" << std::endl;
+                    printJobOffer(*offer);
+                }
+                else {
+                    std::cout << "Offer was not found!" << std::endl;
+                }
+
+                delete offer;
+                break;
+            }
+            default: {
+                return 0;
+            }
+        }
+    }
 }
